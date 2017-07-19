b7bfedb9240e3bb765f608ec018a8abe70bdf2b8
zjj <546060544@qq.com>
2017-07-19 11:39:07 +0800

    python 数据结构总结

--- practise/dict.py
+++ practise/dict.py
@@ -1,15 +1,20 @@
 #!usr/etc/env python3
 # -*- coding:utf-8 -*-
-#python 内置数据结构dict(字典 or assoc_array in php)
-#快速查找，占用内存多浪费
+# python 内置数据结构dict(字典 or assoc_array in php) key为不可变对象
+# 快速查找，占用内存多浪费
 
-score = {'zjj':98,'jxy':100}
+score = {'zjj': 98, 'jxy': 100}
 score['zjj'] = 139
-#获取字典中元素的值dict.get(itme,default)
+# 获取字典中元素的值dict.get(item,default)
 print(score.get('zjj'))
 print(score.get('sq', 0))
 print(score.get('sq'))
 print(score['zjj'])
-#删除一个key
+# 删除一个key
 score.pop('jxy')
 print(score)
+
+# list 有序集合列表 可变 索引数组
+# list 相关函数 len(list) list.pop() list.append(value) list.insert(index,value)
+
+#

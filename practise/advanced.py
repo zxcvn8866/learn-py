ec7f2bd88a29529547c79513d122d2e721887eaf
zjj <546060544@qq.com>
2017-07-21 15:51:53 +0800

    recursive_func and slice

--- practise/advanced.py
+++ practise/advanced.py
@@ -19,4 +19,38 @@ l = list(range(0, 100))
 # slice L[s:e:n] s:开始索引 e:结束索引 n:步数，正负代表方向，有相对应的s和e的缺省值
 print(l[80::5], l[80::-5], sep='\n')
 
+# iteration 迭代
+l = {'name': 'zjj', 'age': 28, 'sex': 1}
+# 迭代 value
+for value in l.values():
+    print(value)
+# 迭代 key
+for key in l:
+    print(key, ':', l[key])
+# 迭代k,v
+for k, v in l.items():
+    print(k, v, sep=':')
+# enumerate 将可迭代的对象转换为一个索引序列，支持第二参数(设置初始索引，默认为0)
+# 为python内置函数，返回enumerate对象
+l = ['abc', 1, 13]
+for k, v in enumerate(l):
+    print(k, v, sep=':')
 
+
+# 判断一个对象是否可迭代
+def judge(x):
+    from collections import Iterable
+    return isinstance(x, Iterable)
+
+
+c = ['abc', 123, [1, 2, 3], (1, 2, 3), set([1, 2, 3])]
+# 对list中的每个元素执行相同操作
+d = [judge(v) for v in c]
+print(d)
+
+# for循环同时引用两个变量 x,y=(1,2) x,y,z=(1,2,3)
+# python的解构赋值 把一个tuple赋给多个变量
+for x, y in [(1, 2), (3, 4), (5, 6)]:
+    print(x, y)
+
+# 列表生成式
